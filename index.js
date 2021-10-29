4c030e2fc29647d6c9f9877f558cbe87c4120209
Nguyễn Thế Sơn <thesonpb@gmail.com>
2021-10-29 15:57:30 +0700

    edit student

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const db = require('./database');
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use("/library", express.static(__dirname + '/library'));
 app.set('view engine', 'pug');
 app.use(express.json());
@@ -10,6 +12,17 @@ app.use(express.json());
 app.get('/', (req, res) => {
     res.sendFile('./view/login.html', { root: __dirname });
 });
+
+// app.get('/all-user', (req, res) => {
+//     db.query(`select matkhau from nguoidung where tennguoidung = 'son@son'`, function(err, data) {
+//         if (err) throw err;
+//         // res.send('user-list', { title: 'User list', userData: data });
+//         // console.log(data);
+//         // res.send(data);
+//         console.log(data);
+//         if (data = { data: '1'}) res.send('success');
+//     })
+// });
 
 app.post('/', (req, res) => {
     const { malop, emailcovan } = req.body;
@@ -20,6 +33,24 @@ app.post('/', (req, res) => {
             console.log(err);
         }
     }
+});
+
+app.post('/login', (req, res) => {
+    let username = req.body.username;
+    let password = req.body.password;
+    console.log(password);
+    db.query(`select matkhau from nguoidung where tennguoidung = '${username}'`, function (err, rows) {
+        if (err) {
+            // console.log('err');
+            throw err;
+        }
+        if (password == rows[0].matkhau) {
+            res.sendFile('./view/student.html', { root: __dirname });
+            console.log('true');
+        }
+        
+    });
+
 });
 
 app.post('/admin/secret/signup', (req, res) => {
@@ -34,6 +65,6 @@ app.post('/admin/secret/signup', (req, res) => {
     }
 })
 
-app.listen(3000, ()=> {
+app.listen(3000, () => {
     console.log('listening on port 3000...');
 });

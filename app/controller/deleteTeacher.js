b4d0442fbc25716bfd0218c4a53f86adfaab87e0
Nguyễn Thế Sơn <thesonpb@gmail.com>
2021-11-30 22:31:02 +0700

    delete teacher for admin

--- app/controller/deleteTeacher.js
+++ app/controller/deleteTeacher.js
@@ -1,9 +1,12 @@
 const db = require("../models/index");
 const Covan = db.Covan;
+const Taikhoan = db.Taikhoan;
 
 const deleteTeacher = async (req, res) => {
     const a = await Covan.findByPk(req.params.email);
     await a.destroy();
+    const acc = await Taikhoan.findByPk(req.params.email);
+    await acc.destroy();
     res.redirect('/my');
 }
 

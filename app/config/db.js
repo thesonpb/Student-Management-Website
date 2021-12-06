38c2170d7249abf48ccd1d0f3f09e605fed8da00
hadat112 <hadat1122001@gmail.com>
2021-12-07 02:23:17 +0700

    Hoàn chỉnh

--- app/config/db.js
+++ app/config/db.js
@@ -2,7 +2,7 @@ module.exports = {
     //For mySql connection
     HOST: "localhost",
     USER: "root",
-    PASSWORD: "2272001az",
+    PASSWORD: "12345678",
     DB: "student_management",
     dialect: "mysql",
     //Optional
